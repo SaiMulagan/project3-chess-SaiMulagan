5f6f8d3f6c1f245f1d56f7e41ff455da59876c30
SaiMulagan <89549431+SaiMulagan@users.noreply.github.com>
2022-05-05 00:09:13 -0700

    Update Bishop.java
    changed a one letter typo

--- Bishop.java
+++ Bishop.java
@@ -7,7 +7,7 @@ public class Bishop extends ChessPiece {
         public boolean move(int toCol, int toRow) {
             System.out.println("BISHOP: (" + getColumn() + "," + getRow() + ") ("+ toCol + "," + toRow + ")");
             int rowDis = Math.abs(toRow - getRow());
-            int coldis = Math.abs(toCol - getColumn());
+            int colDis = Math.abs(toCol - getColumn());
 
                 if(colDis != 0 && rowDis == colDis){
                     return true;}
